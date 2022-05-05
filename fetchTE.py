d9e42587c216d7a581ab70446886b1e50ce49e5f
Taizi Huang <htzllt@gmail.com>
2022-05-06 04:18:47 +0800

    Update fetchTE.py

--- fetchTE.py
+++ fetchTE.py
@@ -1,5 +1,65 @@
-import os,requests,re,time
+import os,requests,re,time,json
 from bs4 import BeautifulSoup
+
+def fetchArticle(link,n=1):
+
+    r = requests.get(link)
+    doc = BeautifulSoup(r.content,features="lxml")
+
+    if not doc.find(class_="article__body-text"):
+        return 'page-not-found'
+
+    if doc.find('iframe'):
+        fetchGraphic(link)
+        return link.split('/')[-1]
+
+    # fetch article body
+    body = doc.find(class_="article__body-text").parent
+    for i in body.findAll('a',{'href':re.compile("^/")}):
+        url = i.attrs['href']
+        if 'https' in url:
+            pass
+        else:
+            url = 'https://www.economist.com'+url
+        if 'email' in url:
+            continue
+        if n!= 0:
+            l = fetchArticle(url,0)
+            i.attrs['href'] = './'+l+'.html'            
+        else:
+            i.attrs['href'] = './'+url.split('/')[-1]+'.html'
+
+    fig_html = ''
+    if doc.find('figure'):
+        src = fetchImg(doc.find('figure').find('img').attrs['src'])
+        fig_html = '<img id="myImg" src=".{}" width="auto" height="200">'.format(src)
+    if body.find('img'):
+        for i in body.findAll("img"):
+            url = i.attrs['src']
+            i.attrs['src'] = '.'+fetchImg(url)
+            i.attrs['srcset'] = '.'+fetchImg(url)
+
+    for i in doc.find('section').findAll('style'):
+        i.decompose()
+    header = doc.find('section')
+
+    html = '<html lang="en"><meta name="viewport" content="width=device-width, initial-scale=1" /><head><!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=G-2VYEP6CXDE"></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date());  gtag("config", "G-2VYEP6CXDE");</script><link rel="stylesheet" href="../init.css"></head><body>'+str(header)+fig_html+str(body)+'</body></html>'
+    htmlname = './html/'+link.split('/')[-1]+'.html'
+    with open(htmlname,'w') as f:
+        f.write(html)
+    print('Fetching: '+ link.split('/')[-1])
+    return link.split('/')[-1]
+
+def fetchImg(url):
+    if not os.path.isdir('image'):
+        os.mkdir('image')
+    if 'https' not in url:
+        url = 'https://www.economist.com/'+url
+    img = requests.get(url).content
+    imgfile = './image/'+url.split('/')[-1]
+    with open(imgfile,"wb") as f:
+        f.write(img)
+    return imgfile
 
 def fetchAI(url):
     r = requests.get(url)
@@ -36,6 +96,7 @@ def fetchAI(url):
                 scriptURL = 'https://www.youtube.com'+scriptURL
             if 'https' not in scriptURL:
                 scriptURL = 'https://infographics.economist.com/2021/news-reviews/'+scriptURL
+            scriptURL = scriptURL.split('?')[0]
             scripttext = requests.get(scriptURL).text
             scriptfile = './assets/'+scriptURL.split('/')[-1]
             with open(scriptfile,'w') as f:
@@ -57,13 +118,14 @@ def fetchGraphic(link):
         if 'https' not in url:
             url = 'https://www.economist.com/'+url
         fetchAI(url)
-    header = doc.find(class_="ds-layout-grid ds-layout-grid--edged layout-article-header")
-    body = doc.find(class_="ds-layout-grid ds-layout-grid--edged layout-article-body")
+    for i in doc.find('section').findAll('style'):
+        i.decompose()
+    header = doc.find('section')
+    body = doc.find(class_="article__body-text").parent
     for i in body.findAll('iframe'):
         indexURL = i.attrs['src']
         # fetchAI(indexURL)
         i.attrs['src'] = indexURL.split('/')[-2]+'_index.html'
-    body.find('aside').decompose()
     js = """<script>
     window.tedl = window.tedl || {};
     // Resize iframes on articles with interactives when they send a RESIZE message
@@ -84,103 +146,31 @@ def fetchGraphic(link):
         f.write(html)
     return
 
-def fetchArticle(link,n=1):
-    r = requests.get(link)
-    doc = BeautifulSoup(r.content,features="lxml")
-    if not doc.find('meta',{'property':"og:url"}):
-        return link
-    link = doc.find('meta',{'property':"og:url"}).attrs['content']
-    body = doc.find(class_="ds-layout-grid ds-layout-grid--edged layout-article-body")
-    if not body:
-        return 'page-not-found'
-    for i in body.findAll('a',{'href':re.compile("^/")}):
-        url = i.attrs['href']
-        if 'https' in url:
-            pass
-        else:
-            url = 'https://www.economist.com'+url
-        if 'email' in url:
-            continue
-        if n!=0:
-            l = fetchArticle(url,0)
-            i.attrs['href'] = './'+l+'.html'            
-        else:
-            i.attrs['href'] = './'+url.split('/')[-1]+'.html'
-    for i in doc.findAll("img"):
-        url = i.attrs['src']
-        if 'https' not in url:
-            url = 'https://www.economist.com/'+url
-        img = requests.get(url).content
-        imgfile = './image/'+url.split('/')[-1]
-        with open(imgfile,"wb") as f:
-            f.write(img)
-        i.attrs['src'] = '../image/'+url.split('/')[-1]
-        i.attrs['srcset'] = '../image/'+url.split('/')[-1]
-    if doc.find(class_="react-audio-player"):
-        audioURL = doc.find(class_="react-audio-player").attrs['src']
-        audio = requests.get(audioURL).content
-        audioname = './audio/'+link.split('/')[-1]+'.mp3'
-        doc.find(class_="react-audio-player").attrs['src'] = '../audio/'+link.split('/')[-1]+'.mp3'
-        with open(audioname,'wb') as f:
-            f.write(audio)
-    if doc.find('iframe'):
-        if 'acast' in doc.find('iframe').attrs['src']:
-            acastname0 = doc.find('iframe').attrs['src'].replace('embed','sphinx')
-            acastname = acastname0.split('/')[-1]
-            if 'https' not in acastname0:
-                acastURL = 'https:'+acastname0+'/media.mp3'
-            else:
-                acastURL = acastname0+'/media.mp3'
-            txt = BeautifulSoup('<audio class="react-audio-player" controls="" controlslist="nodownload" id="audio-player" preload="none" src="../audio/'+acastname+'.mp3" title=""><p></p></audio>',features="lxml")
-            doc.find('iframe').replaceWith(txt)
-            if not os.path.isfile('./audio/'+acastname+'.mp3'):
-                acast = requests.get(acastURL).content
-                with open('./audio/'+acastname+'.mp3','wb') as f:
-                    f.write(acast)
-    title = doc.find(class_="article__headline").text
-    header = doc.find(class_="ds-layout-grid ds-layout-grid--edged layout-article-header")
-    body = doc.find(class_="ds-layout-grid ds-layout-grid--edged layout-article-body")
-    body.find('aside').decompose()
-    html = '<html lang="en"><meta name="viewport" content="width=device-width, initial-scale=1" /><head><!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=G-2VYEP6CXDE"></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date());  gtag("config", "G-2VYEP6CXDE");</script><link rel="stylesheet" href="../init.css"><title>'+title+'</title></head><body>'+str(header)+str(body)+'</body></html>'
-    htmlname = './html/'+link.split('/')[-1]+'.html'
-    with open(htmlname,'w') as f:
-        f.write(html)
-    print('Fetching: '+ title)
-    return link.split('/')[-1]
-
 url = "https://www.economist.com/weeklyedition/"
 r = requests.get(url)
 doc = BeautifulSoup(r.content,features="lxml")
 docu = doc.find(class_='layout-weekly-edition')
 headerImgURL = docu.find(class_='weekly-edition-header__image').find('img').attrs['src']
-graphicURL = docu.find('a',{'href': re.compile("graphic-detail")}).attrs['href']
-if 'https' not in graphicURL:
-    graphicURL = "https://www.economist.com"+graphicURL
 img = requests.get(headerImgURL).content
 with open('./image/cover.png',"wb") as f:
     f.write(img)
 for i in docu.findAll(class_='weekly-edition-wtw__item'):
     link = i.find('a').attrs['href']
-    if 'https' in link:
-        fetchArticle(link)
-    else:
-        fetchArticle("https://www.economist.com"+link)
+    print(link)
+    if 'https' not in link:
+        link = "https://www.economist.com"+link
+    fetchArticle(link)
     i.find('a').attrs['href'] = './html/'+link.split('/')[-1]+'.html'
-    # fetchGraphic(graphicURL)
     time.sleep(2)
 for i in docu.findAll(class_="headline-link"):
     link = i.attrs['href']
-    if link in graphicURL:
-        i.attrs['href'] = './html/'+link.split('/')[-1]+'.html'
-    else:
-        if 'https' in link:
-            fetchArticle(link)
-        else:
-            fetchArticle("https://www.economist.com"+link)
-        i.attrs['href'] = './html/'+link.split('/')[-1]+'.html'
+    if 'https' not in link:
+        link = "https://www.economist.com"+link
+    fetchArticle(link)
+    i.attrs['href'] = './html/'+link.split('/')[-1]+'.html'
     time.sleep(2)
 for i in docu.findAll("img"):
     i.decompose()
-html = '<html lang="en"><meta name="viewport" content="width=device-width, initial-scale=1" /><head><!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=G-2VYEP6CXDE"></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date());  gtag("config", "G-2VYEP6CXDE");</script><link rel="stylesheet" href="init.css"><title>The Economist</title></head><body><img src="./image/cover.png">'+str(docu)+'</body></html>'
+html = '<html lang="en"><meta name="viewport" content="width=device-width, initial-scale=1" /><head><!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=G-2VYEP6CXDE"></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date());  gtag("config", "G-2VYEP6CXDE");</script><link rel="stylesheet" href="init1.css"><title>The Economist</title></head><body><img src="./image/cover.png">'+str(docu)+'</body></html>'
 with open('index.html','w') as f:
     f.write(html)
